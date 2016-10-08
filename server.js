abd5d1c73b909fa1a6c56ae4f7a3daf7fd56ca96
srisharan111 <14bb125@skcet.ac.in>
2016-10-08 20:12:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.ger('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'sharan-one.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
